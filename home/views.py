b4036bc565e2602ea891d4e190798e4c3ff44673
VismayaUnnikrishnan <119035329+VismayaUnnikrishnan@users.noreply.github.com>
2024-02-02 17:05:46 +0530

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from . import models
+from django.http import HttpResponseRedirect,HttpResponse
+
+
+# Create your views here.
+def home_view(request):
+    products=models.Product.objects.all()
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        counter=product_ids.split('|')
+        product_count_in_cart=len(set(counter))
+    else:
+        product_count_in_cart=0
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'ecom/index.html')
